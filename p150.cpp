962ebb2e2c7c60f6e77cdb284fb20bacf23567e2
Yang-33 <kasai.yuta0810@gmail.com>
2017-07-26 00:39:24 +0900

    numeric_limits

---
+++ p150.cpp
@@ -0,0 +1,51 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define FOR(i, s, e) for (ll(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (ll(i) = (s); (i) > (e); (i)--)
+#define debug(x) cout << #x << ": " << x << endl
+#define mp make_pair
+#define pb push_back
+const ll MOD = 1000000007;
+const int INF = 1e9;
+const ll LINF = 1e16;
+const double PI = acos(-1.0);
+int dx[8] = {0, 0, 1, -1, 1, 1, -1, -1};
+int dy[8] = {1, -1, 0, 0, 1, -1, 1, -1};
+
+/* -----  xtimex  Problem: __CONTEST_NAME__  / Link:   ----- */
+/* ------問題------
+
+
+
+-----問題ここまで----- */
+/* -----解説等-----
+
+
+
+----解説ここまで---- */
+
+ll N;
+
+ll ans = 0LL;
+
+int main() {
+  cin.tie(0);
+  ios_base::sync_with_stdio(false);
+
+  cout << ll(numeric_limits<ll>::min()) << endl;
+  cout << ll(numeric_limits<ll>::max()) << endl;
+  cout << int(numeric_limits<char>::min_exponent) << endl;
+  cout << int (numeric_limits<char>::is_signed) << endl;
+  cout << numeric_limits<double>::digits << endl;
+  cout << numeric_limits<double>::radix << endl;
+  cout << numeric_limits<double>::epsilon() << endl;
+  cout << numeric_limits<double>::round_error() << endl;
+  cout << ans << endl;
+
+  return 0;
+}
