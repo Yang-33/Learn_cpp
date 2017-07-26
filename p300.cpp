04a04438dfe868ad0b2ece846de7f82b9d47b485
Yang-33 <kasai.yuta0810@gmail.com>
2017-07-26 21:37:07 +0900

    ポインタ 文字列 位置

---
+++ p300.cpp
@@ -0,0 +1,55 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define FOR(i, s, e) for (ll(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (ll(i) = (s); (i) > (e); (i)--)
+#define debug(x) cout << #x << ": " << x << endl
+#define mp make_pair
+#define pb push_back
+const ll MOD = 1000000007;
+const int INF = 1e9;
+const ll LINF = 1e16;
+const double PI = acos(-1.0);
+int dx[8] = {0, 0, 1, -1, 1, 1, -1, -1};
+int dy[8] = {1, -1, 0, 0, 1, -1, 1, -1};
+
+/* -----  xtimex  Problem: __CONTEST_NAME__  / Link:   ----- */
+/* ------問題------
+
+
+
+-----問題ここまで----- */
+/* -----解説等-----
+
+
+
+----解説ここまで---- */
+
+ll N;
+
+ll ans = 0LL;
+
+int main() {
+  cin.tie(0);
+  ios_base::sync_with_stdio(false);
+
+
+  // 文字列リテラルを指すポインタには、別の文字列リテラルへのポインタを代入できる。
+  // 代入後のポインタは新しく代入された文字列リテラルを指すことになる。
+
+  char *sp = "asdas";
+  cout << sp << endl;
+  sp = "123";
+  cout << sp << endl;
+
+  char s[] = "qwer";
+  cout << s << endl;
+  // s = "1234"; // このような代入は存在しない
+  cout << s << endl;
+
+  return 0;
+}
