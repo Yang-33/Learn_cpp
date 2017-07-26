5023c1795ce732c7f0117bf539c2107ee9ca2748
Yang-33 <kasai.yuta0810@gmail.com>
2017-07-26 18:34:15 +0900

    初期化子と動的生成の初期化子

---
+++ p279.cpp
@@ -0,0 +1,57 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define FOR(i, s, e) for (ll(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (ll(i) = (s); (i) > (e); (i)--)
+#define debug(x) cout << #x << ": " << x << endl
+#define mp make_pair
+#define pb push_back
+const ll MOD = 1000000007;
+const int INF = 1e9;
+const ll LINF = 1e16;
+const double PI = acos(-1.0);
+int dx[8] = {0, 0, 1, -1, 1, 1, -1, -1};
+int dy[8] = {1, -1, 0, 0, 1, -1, 1, -1};
+
+/* -----  xtimex  Problem: __CONTEST_NAME__  / Link:   ----- */
+/* ------問題------
+
+
+
+-----問題ここまで----- */
+/* -----解説等-----
+
+
+
+----解説ここまで---- */
+
+ll N;
+
+ll ans = 0LL;
+
+int main() {
+  cin.tie(0);
+  ios_base::sync_with_stdio(false);
+
+  // 初期化子
+  int a = 5;
+  int b(5);
+  int c = int(5); // すき
+  cout << a << endl;
+  cout << b << endl;
+  cout << c << endl;
+
+  // 動的生成
+  int *x = new int; // うちでは0になったが不定値で初期化
+  cout << *x << endl;
+  int *y = new int(); // 0
+  cout << *y << endl;
+  int *z = new int(5); // 5
+  cout << *z << endl;
+
+  return 0;
+}
