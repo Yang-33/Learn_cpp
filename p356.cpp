48ca9f48ebcb24a76b2f02fc327bd9b902f0618c
Yang-33 <kasai.yuta0810@gmail.com>
2017-07-28 00:34:33 +0900

    公開範囲確認

---
+++ p356.cpp
@@ -0,0 +1,56 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define FOR(i, s, e) for (ll(i) = (s); (i) < (e); (i)++)
+#define FORR(i, s, e) for (ll(i) = (s); (i) > (e); (i)--)
+#define debug(x) cout << #x << ": " << x << endl
+#define mp make_pair
+#define pb push_back
+const ll MOD = 1000000007;
+const int INF = 1e9;
+const ll LINF = 1e16;
+const double PI = acos(-1.0);
+int dx[8] = {0, 0, 1, -1, 1, 1, -1, -1};
+int dy[8] = {1, -1, 0, 0, 1, -1, 1, -1};
+
+/* -----  xtimex  Problem: __CONTEST_NAME__  / Link:   ----- */
+/* ------問題------
+
+
+
+-----問題ここまで----- */
+/* -----解説等-----
+
+
+
+----解説ここまで---- */
+
+
+class aaa {
+	int a;
+public:
+	int b;
+	int c;
+private:
+	int z;
+public:
+	int zz;
+public:
+	aaa(int aa, int aaa, int aaaa, int aaaaa, int aaaaaa) {
+		a = aa; b = aaa; c = aaaa; z = aaaaa; zz = aaaaaa;
+	}
+
+};
+
+int main() {
+  cin.tie(0);
+  ios_base::sync_with_stdio(false);
+
+  aaa A(1,2,3,34,45);
+  cout << /*A.a << */ A.b <<" "<< A.c <<" "<< /* A.z <<*/ A.zz << endl;
+  return 0;
+}
